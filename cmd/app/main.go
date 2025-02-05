77dc06fffa22e86f15cd91ea9000744b3f24a1fb
Aleksandr Chudakov <chudo88@gmail.com>
2025-02-05 16:46:54 +0300

    add ci and ServerMux 1.23

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -11,17 +11,24 @@ import (
 	"github.com/iudanet/go-example-app/internal/worker"
 )
 
+// setupRoutes создает и возвращает настроенный ServeMux.
+func setupRoutes(msgController controller.MessageController) *http.ServeMux {
+	mux := http.NewServeMux()
+	// Регистрация обработчика для маршрута "/"
+	mux.HandleFunc("/", msgController.MessageHandler)
+	return mux
+}
+
 func main() {
 	repo := repository.NewInMemoryMessageRepo()
 	msgService := service.NewMessageService(repo)
 	msgController := controller.NewMessageController(msgService)
 	go worker.Start(repo) // Запуск воркера
-	// HTTP роутинг
-	http.HandleFunc("/", msgController.MessageHandler)
+
 	log.Println("Server is running at :8080")
 	srv := &http.Server{
 		Addr:         ":8080",
-		Handler:      nil,
+		Handler:      setupRoutes(msgController),
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 10 * time.Second,
 		IdleTimeout:  120 * time.Second,
